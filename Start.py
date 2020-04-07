dd67000b5ea9734cb7f71fac1e6e720ca7185bf4
shubhamdhanuka11@gmail.com <shubhamdhanuka11@gmail.com>
2020-04-08 01:24:05 +0530

    Checking

---
+++ Start.py
@@ -0,0 +1,34 @@
+import math;
+#start python journey from here..,
+print("Hi i am good");
+#x = input("Enter any no here")
+#print(x)
+x = 1
+y = 2.0
+z = 'string'
+print(x)
+print(y)
+print(z)
+z ='2'
+print(int(z))
+print(float(z))
+print(str(z))
+print(math.ceil(4.5))
+print(math.floor(4.5))
+print(math.log(2))
+
+print(math.sqrt(81))
+#print(math.round(34.4))
+print(max(2,3))
+print(abs(-3.4))
+print(round(3.4))
+
+
+string = "Hi"
+print(string[0])
+li = ['1','2']
+print(li[0])
+tu =(1,2)
+print(tu[0])
+print(math.exp(2))
+print(math.sin)
